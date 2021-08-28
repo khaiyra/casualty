5311e5291734b9261ba59a1e01c5ab41941b6454
khaiyra <ayindekhairat@gmail.com>
2021-08-28 17:28:26 +0100

    update scripts

--- scripts/plots.py
+++ scripts/plots.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # Count plot
 def plot_count(data:pd.DataFrame,col:str)->None:
     plt.figure(figsize=(13,6.5))
-    sns.countplot(data[col],palette = 'RdYlGn')
+    sns.countplot(data[col])
     plt.title(f'Count of Cancer {col}')
     plt.show()
     
@@ -21,9 +21,9 @@ def plot_pie(data:pd.DataFrame,col:str)->None:
     
 # Scatter plot
 def plot_scatter(df,col1,col2):
-    fig, ax = plt.subplots(2, 2, figsize=(15, 15))
-    #plt.figure(figsize=(13, 6.5))
-    sns.scatterplot(data = df, x=col1, y=col2, hue="diagnosis",palette='magma', ax=ax[0][0])
+    #fig, ax = plt.subplots(2,1, figsize=(15, 15))
+    plt.figure(figsize=(13, 6.5))
+    sns.scatterplot(x=col1, y=col2, hue="diagnosis", data = df, palette='magma')
     plt.title(f'{col1} vs {col2}', size=16)
     plt.xticks(fontsize=14)
     plt.yticks( fontsize=14)
@@ -34,19 +34,19 @@ def plot_heat(data:pd.DataFrame)->None:
     f, ax = plt.subplots(figsize = (20, 20))
     corr = data.corr()
     #plt.figure(figsize=(20,20))
-    sns.heatmap(corr, cbar=True, square= True, fmt='.1f', annot=True, annot_kws={'size':15}, cmap='summer_r')
+    #sns.heatmap(corr, cbar=True, square= True, fmt='.1f', annot=True, annot_kws={'size':15}, cmap='summer_r')
     sns.heatmap(data.corr(), annot = True, linewidth = 0.5, fmt = '.1f', ax = ax )
     #plt.savefig("../figures/correlation.png")
     plt.show()
     
 # Hisogram plot
 def plot_hist(df:pd.DataFrame, column:str, color:str)->None:
-    plt.figure(figsize=(9, 7))
+    plt.figure(figsize=(13, 6.5))
     sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
     plt.title(f'Distribution of {column}', size=20, fontweight='bold')
     plt.show()
     
 #Distribution plot    
 def plot_distribution(data:pd.DataFrame,col:str)->None:
-    sns.FacetGrid(data, hue="diagnosis", height=6,palette='summer_r').map(sns.distplot, col).add_legend()
+    sns.FacetGrid(data, hue="diagnosis", height=6).map(sns.distplot, col).add_legend()
     plt.show()
